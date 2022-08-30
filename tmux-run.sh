4cc18f8b50e3db9db159d481e81216c9d584e671
Massimo Manzato <massimo.manzato@gmail.com>
2022-08-30 10:26:25 +0200

    fix the ip address add if link down/up

--- tmux-run.sh
+++ tmux-run.sh
@@ -4,10 +4,10 @@ session="mmcli_cl_session"
 
 tmux new-session -d -s $session
 
-window=0
+window=1
 tmux rename-window -t $session:$window 'connect_loop'
 tmux send-keys -t $session:$window 'bash /projects/MMA/mmcli-cl/connect-loop' C-m
 
-#window=1
-#tmux new-window -t $session:$window -n 'vim'
-#tmux send-keys -t $session:$window 'vim package.json'
+window=2
+tmux new-window -t $session:$window -n 'logs'
+tmux send-keys -t $session:$window 'dmesg -T'
