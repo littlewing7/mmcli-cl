5e502ccd40a7cd13ee4031a87c5f6af0f4618be1
Massimo Manzato <massimo.manzato@gmail.com>
2022-08-30 10:51:48 +0200

    test tmux runs

--- tmux-run.sh
+++ tmux-run.sh
@@ -6,8 +6,8 @@ tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'connect_loop'
-tmux send-keys -t $session:$window 'bash /projects/MMA/mmcli-cl/connect-loop' C-m
+tmux send-keys -t $session:$window 'bash --init-file /projects/MMA/mmcli-cl/connect-loop' C-m
 
 window=2
 tmux new-window -t $session:$window -n 'logs'
-tmux send-keys -t $session:$window 'dmesg -T'
+tmux send-keys -t $session:$window 'bash --init-file ; dmesg -T'
