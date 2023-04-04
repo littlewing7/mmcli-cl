2bb3673661f4a23d2a67a49264138a4050668024
Massimo Manzato <massimo.manzato@gmail.com>
2023-04-04 12:44:48 +0200

    fix error checks

--- tmux-run.sh
+++ tmux-run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-export PS1="\[\033[94m\][\t] \[\033[01;31m\]\u@\h \[\033[95m\]\w\[\033[94m\] $(__git_ps1 '(%s)')\n#\[\]"
-
+export HOME="/root"
 . "$HOME/.bashrc"
 
 session="mmcli_cl_session"
@@ -10,8 +9,10 @@ tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'connect_loop'
+tmux send-keys -t $session:$window '/usr/bin/bash' C-m
 tmux send-keys -t $session:$window 'bash /projects/MMA/mmcli-cl/connect-loop' C-m
 
 window=2
 tmux new-window -t $session:$window -n 'logs'
-tmux send-keys -t $session:$window 'dmesg -T'
+tmux send-keys -t $session:$window '/usr/bin/bash' C-m
+tmux send-keys -t $session:$window 'dmesg -T' C-m
