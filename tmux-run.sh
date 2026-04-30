4291c277e2b88461fe8470bf7270180a5b777b55
Massimo Manzato <massimo.manzato@gmail.com>
2026-04-30 13:42:29 +0200

    mmcli-cl: added support for ipv4v6 or ipv6 and long options syntax substitution

--- tmux-run.sh
+++ tmux-run.sh
@@ -10,9 +10,21 @@ tmux new-session -d -s $session
 window=1
 tmux rename-window -t $session:$window 'connect_loop'
 tmux send-keys -t $session:$window '/usr/bin/bash' C-m
-tmux send-keys -t $session:$window 'bash /projects/MMA/mmcli-cl/connect-loop datacard.tre.it' C-m
+tmux send-keys -t $session:$window 'bash /projects/MMA/mmcli-cl/connect-loop datacard.tre.it ipv4v6' C-m
 
 window=2
 tmux new-window -t $session:$window -n 'logs'
 tmux send-keys -t $session:$window '/usr/bin/bash' C-m
 tmux send-keys -t $session:$window 'dmesg -T' C-m
+
+window=3
+tmux new-window -t $session:$window -n 'gping'
+tmux send-keys -t $session:$window '/usr/bin/bash' C-m
+tmux send-keys -t $session:$window 'sleep 60' C-m
+tmux send-keys -t $session:$window 'gping 8.8.8.8' C-m
+
+window=4
+tmux new-window -t $session:$window -n 'gping_ipv6'
+tmux send-keys -t $session:$window '/usr/bin/bash' C-m
+tmux send-keys -t $session:$window 'sleep 60' C-m
+tmux send-keys -t $session:$window 'gping www.google.com' C-m
